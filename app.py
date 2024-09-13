601f5d459f4813e6132485aa6a801d6344c81ba0
Yasodya12 <rishikayasodya12@gmail.com>
2024-09-14 00:40:54 +0530

    updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>AWS Assigment5</h1>'
+    return '<h1>AWS Assigment</h1>'
 
 if __name__ == '__main__':
     #app.run(debug=True)
